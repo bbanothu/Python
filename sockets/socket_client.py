b4a8155651636a378a69c5e7cfbf6ea2752f6254
Bharath banothu <bbanothu@iastate.edu>
2020-03-28 20:06:29 -0700

    Socket - Client/Server && TCP - Client/Server

---
+++ sockets/socket_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import socket
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
+while 1:
+
+    MESSAGE = str(raw_input("Enter your first message : "))
+    if MESSAGE == "Quit":
+        s.send(MESSAGE)
+        break;
+    else:
+        s.send(MESSAGE)
+        data = s.recv(BUFFER_SIZE)
+        print data
+
+s.close()
---
+++ test.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+a = True
+b = True
+
+if a == b:
+    print("true")
+
+b = False
+if a != b:
+    print("false")    
